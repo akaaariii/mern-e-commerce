c3e10216d64bba1ef208d1c59301478ba20212da
akaaariii <caaari23@gmail.com>
2021-02-10 23:03:39 -0800

    add heroku

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-
+const dotenv = require('dotenv')
 const products = require('./data/products')
+
+dotenv.config()
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
+
 
 app.get('/api/products', (req, res) => {
   res.json(products)
@@ -18,4 +18,20 @@ app.get('/api/products/:id', (req, res) => {
 })
 
 
-app.listen(5000, () => console.log(`Server running on port 5000`))
+if(process.env.NODE_ENV === 'production'){
+  const path = require('path')
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
