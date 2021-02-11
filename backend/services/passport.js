36e16dd11fe6aa12d4a771fd35e878f907be4829
akaaariii <caaari23@gmail.com>
2021-02-11 13:16:19 -0800

    added passport

---
+++ backend/services/passport.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const keys = require('../config/keys');
+const User = mongoose.model('User');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((user) => {
+      done(null, user)
+    })
+    .catch(err => console.error(err))
+});
+
+// creates a new instance of google strategy
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/api/auth/google/callback',
+  proxy: true
+}, async (acessToken, refreshToken, profile, done) => {
+  const existingUser = await User.findOne({ googleId: profile.id });
+
+  if(existingUser){
+    return done(null, existingUser)
+  }
+  const user = await new User({ googleId: profile.id }).save();
+  done(null, user);
+}));
