b56ecd76e191644408cec86c12be15b086c5e613
akaaariii <caaari23@gmail.com>
2021-02-11 22:10:37 -0800

    added action and reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import './bootstrap.min.css';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import App from './App';
+import reducers from './reducers';
+
+
+const middleware = process.env.NODE_ENV === 'production' ? [thunk] : [thunk, logger];
+
+const store = createStore(reducers, {}, applyMiddleware(...middleware));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
