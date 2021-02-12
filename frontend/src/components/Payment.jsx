a3e8ef500bec15589fb61d643708ef7c443d8248
akaaariii <caaari23@gmail.com>
2021-02-12 11:45:02 -0800

    added checkout

---
+++ frontend/src/components/Payment.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
+
+const Payment = ({ handleToken, cart, totalPrice }) => {
+  return (
+    <StripeCheckout
+      name="NatLeather"
+      description="Please proceed your payment"
+      amount={totalPrice * 100}
+      token={(token) => handleToken(token)}
+      stripeKey={process.env.REACT_APP_STRIPE_PUB_KEY}
+    >
+      <button type="button" className="btn btn-dark btn-block" disabled={cart.cartItems === 0}>
+        Place Order
+      </button>
+    </StripeCheckout>
+  )
+}
+
+export default connect(null, actions)(Payment)
