bf71d952ca8715ef977a25d48960e672214fcf63
akaaariii <caaari23@gmail.com>
2021-02-22 05:11:54 -0800

    added responsive layout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { Navbar } from 'react-bootstrap';
 
 const Header = ({ auth }) => {
   const [userState, setUserState] = useState(null);
@@ -13,30 +14,33 @@ const Header = ({ auth }) => {
 
   return (
     <header>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <Navbar bg="primary" variant="dark" expand="lg">
         <div className="container">
           <Link className="navbar-brand" to="/">NatLeather</Link>
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/cart"><i className="fas fa-shopping-cart"></i> Cart</Link>
-            </li>
-
-            {userState ? (
-              <li className="nav-item">
-                <a className="nav-link" href="/api/auth/logout"><i className="fas fa-user"></i> Logout</a>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">Home</Link>
               </li>
-            ) : (
               <li className="nav-item">
-                <Link className="nav-link" to="/login"><i className="fas fa-user"></i> Login</Link>
+                <Link className="nav-link" to="/cart"><i className="fas fa-shopping-cart"></i> Cart</Link>
               </li>
-            )}
-            
-          </ul>
+  
+              {userState ? (
+                <li className="nav-item">
+                  <a className="nav-link" href="/api/auth/logout"><i className="fas fa-user"></i> Logout</a>
+                </li>
+              ) : (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login"><i className="fas fa-user"></i> Login</Link>
+                </li>
+              )}
+              
+            </ul>
+          </Navbar.Collapse>
         </div>
-      </nav>
+      </Navbar>
     </header>
   )
 }
