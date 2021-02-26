39042f620896a43d61e8aa3fa25e658b0620c3a2
akaaariii <caaari23@gmail.com>
2021-02-26 03:13:38 -0800

    added globalstyle

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import './bootstrap.min.css';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 import App from './App';
