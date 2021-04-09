26f1b10902fc882bedeafdb542d658122fe911b3
akaaariii <caaari23@gmail.com>
2021-04-09 03:52:02 -0700

    modified userModel

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,22 +1,22 @@
-const bcrypt = require('bcryptjs')
+// const bcrypt = require('bcryptjs')
 
-const users = [
-  {
-    name: 'Admin User',
-    email: 'admin@mail.com',
-    password: bcrypt.hashSync('123456', 10),
-    isAdmin: true
-  },
-  {
-    name: 'John Doe',
-    email: 'john@mail.com',
-    password: bcrypt.hashSync('123456', 10),
-  },
-  {
-    name: 'Jane Brown',
-    email: 'jane@mail.com',
-    password: bcrypt.hashSync('123456', 10),
-  },
-]
+// const users = [
+//   {
+//     name: 'Admin User',
+//     email: 'admin@mail.com',
+//     password: bcrypt.hashSync('123456', 10),
+//     isAdmin: true
+//   },
+//   {
+//     name: 'John Doe',
+//     email: 'john@mail.com',
+//     password: bcrypt.hashSync('123456', 10),
+//   },
+//   {
+//     name: 'Jane Brown',
+//     email: 'jane@mail.com',
+//     password: bcrypt.hashSync('123456', 10),
+//   },
+// ]
 
-module.exports = users
+// module.exports = users
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,31 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const userSchema = new Schema(
-  {
+const userSchema = new Schema({
     googleId: String,
-    name: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    isAdmin: {
-      type: Boolean,
-      required: true,
-      default: false
-    }
-  },
-  {
-    timestamps: true
-  }
-)
+})
 
 module.exports = mongoose.model('User', userSchema);
