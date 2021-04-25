332536a5f7b1673ab260c408805dfe2994c19290
akaaariii <caaari23@gmail.com>
2021-04-24 19:00:54 -0700

    Heroku redirect url

--- backend/index.js
+++ backend/index.js
@@ -39,10 +39,9 @@ app.use('/api/stripe', billingRoutes);
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '/../frontend/build')))
-  // app.use(express.static('frontend/build'))
   
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/../frontend/build/index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   })
 } else {
   app.get('/', (req, res) => {
