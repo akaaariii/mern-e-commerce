4eb227bd4bb08ef73342b4872bb2caefc58a74df
Akari Tsutsui <64046039+akaaariii@users.noreply.github.com>
2023-05-07 18:40:59 +0900

    modify: centering

--- frontend/src/pages/Loginpage.jsx
+++ frontend/src/pages/Loginpage.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Loginpage = () => {
   return (
-    <>
-      <h2 className="text-center">Not logged in, please log in to continue</h2>
+    <Wrapper>
+      <h2 className="text-center">Not logged in, please log in to continue!</h2>
       <div className="text-center"><a href="/api/auth/google" className="text-warning">Login with Google</a></div>
-    </>
+    </Wrapper>
   )
 }
 
 export default Loginpage
+
+const Wrapper = styled.div`
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
